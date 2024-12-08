acd648a110922827fd9983a6ce8368f5e97de057
malsinim <malsini.masachchige@dcmail.ca>
2024-12-08 17:17:52 -0500

    commit

--- my-strapi-project/src/api/lab/routes/lab.ts
+++
@@ -1,7 +0,0 @@
-/**
- * lab router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::lab.lab');
--- my-strapi-project/src/api/lab/services/lab.ts
+++
@@ -1,7 +0,0 @@
-/**
- * lab service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::lab.lab');
